799570862a07112d0be78f6ebccb46cec7ab9a18
Sean Genabe <seangenabe@outlook.com>
2015-07-28 11:30:36 +0800

    added tests; removed console logs (sorry!)

--- src/delayer.js
+++ src/delayer.js
@@ -20,15 +20,12 @@ class Delayer {
   }
 
   static async console() {
-    console.log(1)
     var args = process.argv.slice(2)
     if (args.length === 0) return
     var delay = parseInt(args[0])
     if (delay < 0) return
     var d = new Delayer(delay)
-    console.log(2)
     await d.promise
-    console.log(3)
   }
 
 }
