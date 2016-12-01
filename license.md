833ed12f17d7feaf5f584603e70704abfe2f0d88
Sean Genabe <seangenabe@outlook.com>
2016-12-02 02:56:05 +0800

    derive from Promise (fixes #1)
    added eslint
    added node versions to travis
    update license
    update test

--- license.md
+++ license.md
@@ -1,6 +1,6 @@
 The MIT License (MIT)
 
-Copyright (c) 2015 Sean Marvi Oliver Genabe
+Copyright (c) 2015-2016 Sean Marvi Oliver Genabe
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
---
+++ test/api.test.js
@@ -0,0 +1,64 @@
+const test = require('ava')
+const Delayer = require('..')
+const Promise = require('./promise') // https://github.com/avajs/ava/issues/947
+
+const promiseSpecies = Object.getOwnPropertyDescriptor(Promise, Symbol.species)
+const speciesSupport = 'get' in promiseSpecies && Promise[Symbol.species] === Promise
+
+test('create a new instance of Delayer', t => {
+  let d = new Delayer(4)
+  // ok to create new instance
+  t.is(typeof d, 'object')
+  t.true(d instanceof Delayer)
+  if (speciesSupport) {
+    t.true(Delayer.prototype instanceof Promise)
+    t.true(d instanceof Promise)
+  }
+  t.is(d.delay, 4)
+  t.true(d.promise instanceof Promise)
+  // not ok to be called as a function
+  t.throws(() => Delayer(4))
+})
+
+test('can be created without arguments', t => {
+  let d = new Delayer()
+  t.true(d.delay === 0)
+})
+
+if (speciesSupport) {
+
+  test('species features', t => {
+    let d = new Delayer()
+    t.is(d.promise, d)
+    t.is(Delayer[Symbol.species], Promise)
+  })
+
+}
+
+test('resolves after the specified amount of time', async t => {
+  const DURATION = 200
+  let time = process.hrtime()
+  let d = new Delayer(DURATION)
+  await d.promise
+  let diff = process.hrtime(time)
+  t.true(diff[0] < 5)
+})
+
+test('should be cancellable', async t => {
+  const SHORT_DURATION = 200
+  const LONG_DURATION = 1000
+  let time = process.hrtime()
+  let d = new Delayer(LONG_DURATION)
+  setTimeout(() => {
+    d.cancel()
+  }, SHORT_DURATION)
+  try {
+    await d.promise
+    t.fail()
+  }
+  catch (err) {
+    let diff = process.hrtime(time)
+    t.true(diff[0] < 5)
+    t.true(err instanceof Error && err.message === "Delayer cancelled.")
+  }
+})
